7002fb140ea61ae8bc56de762bd750593acaaa47
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-03-26 21:56:41 -0300

    :sparkles: feat: Create user interaction using console

---
+++ Calculadora.Interface/CalculadoraInterface.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bem vindo a Calculadora em C#! \n");
+
+        Console.WriteLine("Escolha uma operação para ser realizada");
+        Console.WriteLine("[1] Soma | [2] Subtrair | [3] Multiplicar | [4] Dividir");
+        string operacao = Console.ReadLine()!;
+
+        Console.WriteLine("Digite o primeiro número a ser utilizado na operação");
+        double n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número a ser utilizado na operação");
+        double n2 = Convert.ToDouble(Console.ReadLine());
+
+        double res = Calculadora.Operacoes.ExecutarCalculo(operacao, n1, n2);
+
+        Console.WriteLine("O resultado da operação é " + res);
+    }
+}
--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -2,24 +2,46 @@
 {
     public static class Operacoes
     {
-        public static int Somar(int n1, int n2)
+        private static double Somar(double n1, double n2)
         {
             return n1 + n2;
         }
 
-        public static int Subtrair(int n1, int n2)
+        private static double Subtrair(double n1, double n2)
         {
             return n1 - n2;
         }
 
-        public static int Multiplicar(int n1, int n2)
+        private static double Multiplicar(double n1, double n2)
         {
             return n1 * n2;
         }
 
-        public static int Dividir(int n1, int n2)
+        private static double Dividir(double n1, double n2)
         {
             return n1 / n2;
+        }
+
+
+        public static double ExecutarCalculo(string operacao, double n1, double n2)
+        {
+            switch (operacao)
+            {
+                case "1":
+                    return Somar(n1, n2);
+
+                case "2":
+                    return Subtrair(n1, n2);
+
+                case "3":
+                    return Multiplicar(n1, n2);
+
+                case "4":
+                    return Dividir(n1, n2);
+
+                default:
+                    throw new Exception("Opção inválida");
+            }
         }
     }
 }
